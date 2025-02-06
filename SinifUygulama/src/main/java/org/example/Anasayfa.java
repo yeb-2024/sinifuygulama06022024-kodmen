424b61bf9e1500dcf258a55533332e133d79b079
bedirhan ergün <bedirhan1029@gmail.com>
2025-02-06 18:39:17 +0300

    profile buttonu eklendi

--- SinifUygulama/src/main/java/org/example/Anasayfa.java
+++ SinifUygulama/src/main/java/org/example/Anasayfa.java
@@ -5,9 +5,16 @@ import javax.swing.*;
 public class Anasayfa {
 
     JFrame frame = new JFrame();
+    JButton button = new JButton("Profile göster");
     Anasayfa(){
+
+        button.setBounds(200,200,100,50);
+        button.setFocusable(false);
+        frame.add(button);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
         frame.setVisible(true);
     }
 }
