70de5c95293e9abded09c1cec9a15271cdf669d6
bedirhan ergün <bedirhan1029@gmail.com>
2025-02-06 18:47:24 +0300

    Profile sayfası oluşturuldu ve buttona tıklanma eklendi

--- SinifUygulama/src/main/java/org/example/Anasayfa.java
+++ SinifUygulama/src/main/java/org/example/Anasayfa.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Anasayfa {
+public class Anasayfa implements ActionListener {
 
     JFrame frame = new JFrame();
     JButton button = new JButton("Profile göster");
@@ -10,11 +12,20 @@ public class Anasayfa {
 
         button.setBounds(200,200,100,50);
         button.setFocusable(false);
+        button.addActionListener(this);
         frame.add(button);
 
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == button){
+            ProfileSayfası profileSayfası = new ProfileSayfası();
+        }
     }
 }
---
+++ SinifUygulama/src/main/java/org/example/ProfileSayfası.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class ProfileSayfası {
+    JFrame frame = new JFrame();
+
+    ProfileSayfası(){
+        frame.setTitle("Profile Sayfası");
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
